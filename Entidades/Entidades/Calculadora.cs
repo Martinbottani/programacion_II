dcdab319c027bda9cb8d389fcee2ae6ccf242204
Martinbottani <40809101+Martinbottani@users.noreply.github.com>
2019-04-10 17:40:07 -0300

    TP1 Comienzo

---
+++ Entidades/Entidades/Calculadora.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Calculadora
+    {
+        private static string ValidarOperador(string operador)
+        {
+            string retorno;
+
+            if (operador == "+" || operador == "-" || operador == "*" || operador == "/")
+            {
+                retorno = operador;
+            }
+            else
+            {
+                retorno = "+";
+            }
+
+            return retorno;
+        }
+
+        public double Operar(Numero num1, Numero num2, string operador)
+        {
+            double result = 0;
+            string aux;
+            aux = Calculadora.ValidarOperador(operador);
+
+            if (aux == "+")
+            {
+                result = num1 + num2;
+            }
+            else if (aux == "-")
+            {
+                result = num1 - num2;
+            }
+            else if (aux == "*")
+            {
+                result = num1 * num2;
+
+            }
+            else if (aux == "/")
+            {
+                result = num1 / num2;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Entidades/Entidades/Numero.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Numero
+    {
+        private double numero;
+
+        public Numero()
+        {
+            this.numero = 0;
+        }
+
+        public Numero(double numero)
+        {
+            this.numero = numero;
+        }
+
+        public Numero(string strNumero)
+        {
+            this.numero = Convert.ToDouble(strNumero);
+        }
+
+        private string SetNumero
+        {
+            set { numero = ValidarNumero(value); }
+        }
+
+        public static double ValidarNumero(string strNumero)
+        {
+            double retorno = 0;
+            int i;
+            bool flag = true;
+            for(i = 0; i < strNumero.Length; i++)
+            {
+                if (strNumero[i] < '0' || strNumero[i] > '9')
+                {
+                    retorno = 0;
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag == true)
+            {
+                double.TryParse(strNumero, out retorno);
+            }
+            return retorno;
+        }
+
+        public static double operator +(Numero n1, Numero n2)
+        {
+            double result;
+            result = n1.numero + n2.numero;
+            return result;
+        }
+
+        public static double operator -(Numero n1, Numero n2)
+        {
+            double result;
+            result = n1.numero - n2.numero;
+            return result;
+        }
+
+        public static double operator *(Numero n1, Numero n2)
+        {
+            double result;
+            result = n1.numero * n2.numero;
+            return result;
+        }
+
+        public static double operator /(Numero n1, Numero n2)
+        {
+            double result;
+            result = n1.numero / n2.numero;
+            return result;
+        }
+    }
+}
+
