13c6181c24055a9d97b1c3525e9318544a20d658
Martinbottani <martinbottani@gmail.com>
2019-05-02 20:18:18 -0300

    Pre-Parcial 2

---
+++ Bottani.Martin/Entitades/Automovil.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entitades
+{
+    public class Automovil : Vehiculo
+    {
+        private ConsoleColor color;
+        private static int valorHora;
+
+        static Automovil()
+        {
+            valorHora = 50;
+        }
+        
+        public Automovil(string patente, ConsoleColor color) : base(patente)
+        {
+            this.color = color;
+        }
+
+        public Automovil(string patente, ConsoleColor color, int valorHora) : base(patente)
+        {
+            this.color = color;
+            Automovil.valorHora = valorHora;
+        }
+
+        public override bool Equals(object obj)
+        {          
+            return obj is Automovil;
+        }
+
+        public override string ConsultarDatos()
+        {
+            StringBuilder mostrar = new StringBuilder();
+            mostrar.AppendFormat("\n{0}\nColor: {1}\nValor de la hora: {2}\n", base.ImprimirTicket(), color, valorHora);
+            return mostrar.ToString();
+        }
+
+    }
+}
---
+++ Bottani.Martin/Entitades/Vehiculo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entitades
+{
+    public abstract class Vehiculo
+    {
+        protected DateTime ingreso;
+        private string patente;
+
+        public string Patente
+        {
+            get
+            {
+                return patente;
+            }
+            set
+            {
+                if(patente.Length == 6)
+                {
+                    this.patente = value;
+                }          
+            }
+        }
+
+        public Vehiculo(string patente)
+        {
+            Patente = patente;
+            ingreso = DateTime.Now.AddHours(-3);
+        }
+
+        public abstract string ConsultarDatos();
+
+        public virtual string ImprimirTicket()
+        {
+            StringBuilder mostrar = new StringBuilder();
+            mostrar.AppendFormat("Ingreso: {0}\n", ingreso);
+            mostrar.AppendLine(this.ToString());
+            return mostrar.ToString();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder mostrar = new StringBuilder();
+            mostrar.AppendFormat("Patente {0}", Patente);
+            return mostrar.ToString();
+        }
+
+        public static bool operator !=(Vehiculo v1, Vehiculo v2)
+        {
+            bool retorno = false;
+            if(v1.Equals(v2) && v1.Patente != v2.Patente)
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+
+        public static bool operator ==(Vehiculo v1, Vehiculo v2)
+        {
+            return !(v1 != v2);
+        }
+    }
+}
