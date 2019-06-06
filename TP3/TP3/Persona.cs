9b282946d96f7e4e97dda3d3941da2bf64a77406
Martinbottani <martinbottani.13@gmail.com>
2019-06-05 22:10:32 -0300

    TP3 PARA SEGUIR

---
+++ TP3/TP3/Persona.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP3
+{
+    public abstract class Persona
+    {
+        public enum ENacionalidad
+        {
+            Argentino, Extranjero
+        }
+        private string apellido;
+        private int dni;
+        private ENacionalidad nacionalidad;
+        private string nombre;
+
+        public abstract string Apellido
+        {
+            get;
+            set;
+        }
+
+        public abstract int DNI
+        {
+            get;
+            set;
+        }
+
+        public abstract ENacionalidad Nacionalidad
+        {
+            get;
+            set;
+        }
+
+        public abstract string Nombre
+        {
+            get;
+            set;
+        }
+
+        public abstract string StringToDNI
+        {
+            set;
+        }
+    }
+}
